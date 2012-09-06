8bf44cb45150afa27fa76ccf6ad41496bc6d4929
borealkiss <info@boreal-kiss.com>
2012-09-06 19:39:09 +0200

    Initial import

---
+++ JoystickController.cpp
@@ -0,0 +1,170 @@
+#include "Arduino.h"
+#include "JoystickController.h"
+
+enum PINS{
+  PIN_ANALOG_X     = 0,
+  PIN_ANALOG_Y     = 1,
+  PIN_BUTTON_UP    = 4,
+  PIN_BUTTON_DOWN  = 5,
+  PIN_BUTTON_LEFT  = 6,
+  PIN_BUTTON_RIGHT = 3
+};
+
+JoystickController::JoystickController(int low, int high){
+  _threshouldLow = low;
+  _threshouldHigh = high;
+  this->_setup();
+}
+
+void JoystickController::update(){
+  this->_reset();
+  this->_updateDirection();
+  this->_updateButtons();
+}
+
+int JoystickController::getDirection(){
+  return _direction; 
+}
+
+bool JoystickController::upButtonPressed(){
+  return _upButtonPressed;
+}
+
+bool JoystickController::downButtonPressed(){
+  return _downButtonPressed;
+}
+
+bool JoystickController::leftButtonPressed(){
+  return _leftButtonPressed;
+}
+
+bool JoystickController::rightButtonPressed(){
+  return _rightButtonPressed;
+}
+
+/**
+ * Protected.
+ */
+void JoystickController::_reset(){
+  _direction = DIRECTION_NONE;
+  _upButtonPressed     = false;
+  _downButtonPressed   = false;
+  _leftButtonPressed   = false;
+  _rightButtonPressed  = false; 
+}
+
+/**
+ * Protected.
+ */
+void JoystickController::_setup(){
+  pinMode(PIN_BUTTON_UP, INPUT);
+  digitalWrite(PIN_BUTTON_UP, HIGH);
+  
+  pinMode(PIN_BUTTON_DOWN, INPUT);
+  digitalWrite(PIN_BUTTON_DOWN, HIGH);
+  
+  pinMode(PIN_BUTTON_LEFT, INPUT);
+  digitalWrite(PIN_BUTTON_LEFT, HIGH);
+  
+  pinMode(PIN_BUTTON_RIGHT, INPUT);
+  digitalWrite(PIN_BUTTON_RIGHT, HIGH);
+  
+  this->_reset();
+}
+
+/**
+ * Protected.
+ */
+void JoystickController::_updateDirection(){
+  const int NONE     = 0;
+  const int POSITIVE = 1;
+  const int NEGATIVE = -1;
+  
+  int x = analogRead(PIN_ANALOG_X);
+  int y = analogRead(PIN_ANALOG_Y);
+  
+  int directX = NONE;
+  int directY = NONE;
+  
+  if (x < _threshouldLow){
+    directX = NEGATIVE;
+  }
+  
+  if (x > _threshouldHigh){
+    directX = POSITIVE;
+  }
+  
+  if (y < _threshouldLow){
+    directY = NEGATIVE;
+  }
+  
+  if (y > _threshouldHigh){
+    directY = POSITIVE;
+  }
+  
+  if (directX == NONE){
+    if (directY == POSITIVE){
+      _direction = DIRECTION_UP;
+    }
+    if (directY == NEGATIVE){
+      _direction = DIRECTION_DOWN;
+    }
+  }
+  
+  if (directX == POSITIVE){
+    if (directY == NONE){
+      _direction = DIRECTION_RIGHT;
+    }
+    if (directY == POSITIVE){
+      _direction = DIRECTION_RIGHT_UP;
+    }
+    if (directY == NEGATIVE){
+      _direction = DIRECTION_RIGHT_DOWN;
+    }
+  }
+  
+  if (directX == NEGATIVE){
+    if (directY == NONE){
+      _direction = DIRECTION_LEFT;
+    }
+    if (directY == POSITIVE){
+      _direction = DIRECTION_LEFT_UP;
+    }
+    if (directY == NEGATIVE){
+      _direction = DIRECTION_LEFT_DOWN;
+    }
+  }
+}
+
+/**
+ * Protected.
+ */
+void JoystickController::_updateButtons(){
+  int U = digitalRead(PIN_BUTTON_UP);
+  int D = digitalRead(PIN_BUTTON_DOWN);
+  int L = digitalRead(PIN_BUTTON_LEFT);
+  int R = digitalRead(PIN_BUTTON_RIGHT);
+    
+  if (this->_buttonPressed(U)){
+    _upButtonPressed = true;
+  }
+    
+  if (this->_buttonPressed(D)){
+    _downButtonPressed = true;
+  }
+    
+  if (this->_buttonPressed(L)){
+    _leftButtonPressed = true;
+  }
+    
+  if (this->_buttonPressed(R)){
+    _rightButtonPressed = true;
+  }
+}
+
+/**
+ * Protected.
+ */
+bool JoystickController::_buttonPressed(int state){
+  return (state == LOW); 
+}
---
+++ README.md
@@ -0,0 +1,48 @@
+# JoystickController
+
+Fully compatible with SparkFun's [Arduino joystick shield](https://www.sparkfun.com/products/9760?). The class lets you easily fetch the current status of the joystick. Implementation is quite simple;
+
+	JoystickController(int low = DEFAULT_LOW, int high = DEFAULT_HIGH)
+
+is a constructor. The first argument specifies the lower threshold value for the directional key pad, below which the controller is assumed to set left/down in the horizontal/vertical direction. The second argument specifies the upper threshold value, above which the controller is assumed to set right/up in the horizontal/vertical direction. If arguments are not specified, the following default values will be used:
+
+	static const int DEFAULT_LOW   = 490;
+	static const int DEFAULT_HIGH  = 530;
+
+For more information about the threshold see the following article; [Joystick Shield Quickstart Guide: How do I find the current direction of the joystick?](http://www.sparkfun.com/tutorials/171#direction).
+
+	void update()
+
+must get called in the main loop in order for the class to communicate with the joystick.
+
+	int getDirection()
+
+returns the current direction of the directional keypad as integer values that are defined as class static variables:
+
+	static const int DIRECTION_NONE       = 0;
+	static const int DIRECTION_UP         = 1;
+	static const int DIRECTION_RIGHT_UP   = 2;
+	static const int DIRECTION_RIGHT      = 3;
+	static const int DIRECTION_RIGHT_DOWN = 4;
+	static const int DIRECTION_DOWN       = 5;
+	static const int DIRECTION_LEFT_DOWN  = 6;
+	static const int DIRECTION_LEFT       = 7;
+	static const int DIRECTION_LEFT_UP    = 8;
+
+Finally buttons' status can be fetched from the following functions:
+
+	bool upButtonPressed()
+	bool downButtonPressed()
+	bool leftButtonPressed()
+	bool rightButtonPressed()
+
+They return true if a button is being pressed, otherwise false.
+
+## License
+
+This project is provided under the terms of the [MIT License](http://www.opensource.org/licenses/mit-license.php).
+
+## Contact
+
+* [http://blog.boreal-kiss.net/](http://blog.boreal-kiss.net/)
+* [http://twitter.com/borealkiss](http://twitter.com/borealkiss)
